5b135f433c3d7c0dc018ebc08932d33e5af6fea9
MochArisandiJayanto <diajaya0@gmail.com>
2021-12-01 00:21:42 +0700

    Code App Restaurant

---
+++ lib/provider/restaurant_search_provider.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_dua/data/api_service.dart';
+import 'package:flutter_dua/data/restaurant_search.dart';
+
+enum ResultState { loading, noData, hasData, error }
+
+class RestaurantSearchProvider extends ChangeNotifier {
+  final ApiService apiService;
+  RestaurantSearchProvider({required this.apiService});
+
+  List<Restaurant> _restaurant = [];
+  String _message = '';
+
+  String get message => _message;
+
+  List<Restaurant> get result => _restaurant;
+
+  Future<dynamic> searchRestaurant(String query) async {
+    try {
+      notifyListeners();
+      final restaurant = await apiService.restaurantSearch(query);
+      if (restaurant.isEmpty) {
+        notifyListeners();
+        return _message = 'Empty Data';
+      } else {
+        notifyListeners();
+        return _restaurant = restaurant;
+      }
+    } catch (e) {
+      notifyListeners();
+      return _message = "Can't find restaurant";
+    }
+  }
+}
---
+++ lib/widgets/search.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dua/theme//theme.dart';
+
+class Search extends StatefulWidget {
+  final String query;
+  final ValueChanged<String> onChanged;
+  final String hintText;
+
+  const Search({
+    Key? key,
+    required this.query,
+    required this.onChanged,
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  _SearchState createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final styleActive = TextStyle(color: black);
+    final styleHint = TextStyle(color: grey);
+    final style = widget.query.isEmpty ? styleHint : styleActive;
+
+    return Container(
+      height: 45,
+      margin: const EdgeInsets.only(bottom: 10, top: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.white,
+        border: Border.all(color: green),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          icon: Icon(Icons.search, color: style.color),
+          suffixIcon: widget.query.isNotEmpty
+              ? GestureDetector(
+                  child: Icon(Icons.close, color: style.color),
+                  onTap: () {
+                    controller.clear();
+                    widget.onChanged('');
+                    FocusScope.of(context).requestFocus(FocusNode());
+                  },
+                )
+              : null,
+          hintText: widget.hintText,
+          hintStyle: style,
+          border: InputBorder.none,
+        ),
+        style: style,
+        onChanged: widget.onChanged,
+      ),
+    );
+  }
+}
